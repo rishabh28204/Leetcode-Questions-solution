7ed23f75129511bc6f196303f570be17393becfd
rishabh28204 <108271042+rishabh28204@users.noreply.github.com>
2022-12-30 02:43:52 +0530

    Create reverse linked List.cpp

---
+++ LinkedLists/reverse linked List.cpp
@@ -0,0 +1,49 @@
+// Recursive Code
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL||head->next==NULL)return head;
+        ListNode*res=reverseList(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return res;
+    }
+};
+
+
+
+// iterative code
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* next=NULL,*prev=NULL,*curr=head;
+        while(curr!=NULL)
+        {
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+};
